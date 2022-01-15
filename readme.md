158463734c29ed6b08278cc4d6988225dee1f41e
ikhide <ikhide.atakpu@gmail.com>
2022-01-15 21:49:52 +0100

    mint and burn functions

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-### An ERC-20 smart contract
+### An ERC-20 smart contract named after our dog, Simba
--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -1,17 +1,15 @@
 from scripts.helpful_scripts import get_account
 from brownie import MyToken, network, config
 from web3 import Web3
-import time
-
 
 
 def deploy():
     """
-    Deploys the contract and prints the address
+    Deploys the contract and print the address
     """
 
     account = get_account()
-    initial_supply = Web3.toWei(1000, 'ether')
+    initial_supply = Web3.toWei(1000000000, 'ether')
     token = MyToken.deploy(initial_supply, {'from':account},publish_source=config['networks'][network.show_active()]['verify'])
     print(token.address)
     print(token.totalSupply())
